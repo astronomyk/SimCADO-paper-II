365ac68ae7a4c4e333d5aa0542f529f4bae289f3
Kieran <kdleschinski@gmail.com>
2020-04-08 14:40:56 +0200

    code to plot cluster images

--- project/images_of_clusters/make_plot_2.py
+++ project/images_of_clusters/make_plot_2.py
@@ -1,50 +1,104 @@
+from copy import deepcopy
 import numpy as np
 
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 
 import simcado as sim
-from project.simcado_sandbox import imf
-
-exptime = 3600
-ndit = 1
-
-cmd = sim.UserCommands()
-cmd["OBS_EXPTIME"] = exptime
-cmd["SCOPE_PSF_FILE"] = "PSF_MAORY_SCAO_Ks_2.fits"
-cmd["INST_FILTER_TC"] = "Ks"
-cmd["FPA_LINEARITY_CURVE"] = "none"
-cmd["FPA_USE_NOISE"] = "no"
-cmd["FPA_CHIP_LAYOUT"] = "small"
-
-opt = sim.OpticalTrain(cmd)
-fpa = sim.Detector(cmd, small_fov=False)
-
-mass = 1E3
-lims = [[0.01, 0.08], [0.08, 0.5], [0.5, 300]]
-alphas = [0.3, 1.3, 2.3]
-masses = imf.imf_population(mass=mass * 8 / 5, lims=lims, alphas=alphas)
-lums = imf.luminosity_from_mass(masses)
-
-border = 4
-width = 500
-x, y = np.random.randint(border, width - border, (2, len(lums)))
-xs = 0.004 * (x - width // 2)  # / (dist / 8.5E3)
-ys = 0.004 * (y - width // 2)  # / (dist / 8.5E3)
-
-d = np.array([8.5E3, 20E3, 50E3, 200E3, 800E3])
-dist_mods = 5 * np.log10(d) - 5
 
 
-for dist_mod in dist_mods:  # GC=14.5, LMC=18.5, Leo I=21.5, M31=24.5
+def make_cluster_hdus(mass=2E4, half_light_radius=1, exptime=3600, show=False):
 
-    mags = imf.abs_mag_from_mass(masses) + dist_mod
-    dist = 10 ** (1 + dist_mod / 5)
+    cmd = sim.UserCommands()
+    cmd["OBS_EXPTIME"] = exptime
+    # cmd["SCOPE_PSF_FILE"] = "PSF_MAORY_SCAO_Ks_2.fits"
+    cmd["INST_FILTER_TC"] = "Ks"
+    cmd["FPA_LINEARITY_CURVE"] = "none"
+    cmd["FPA_USE_NOISE"] = "no"
+    cmd["FPA_CHIP_LAYOUT"] = "FPA_chip_layout_centre.dat"
 
-    src = sim.source.stars(mags=mags, x=xs, y=ys)
-    src.apply_optical_train(opt, fpa)
+    opt = sim.OpticalTrain(cmd)
+    fpa = sim.Detector(cmd, small_fov=False)
 
-    fpa.read_out(OBS_EXPTIME=exptime, filename=f"{dist}pc_{mass}Msun.fits")
+    dist_orig = 1E3
+    cluster = sim.source.cluster(mass=mass, distance=dist_orig, half_light_radius=half_light_radius)
+
+    x_orig = deepcopy(cluster.x)
+    y_orig = deepcopy(cluster.y)
+    f_orig = deepcopy(cluster.weight)
+
+    d = np.array([8.5E3, 20E3, 50E3, 200E3, 800E3])
+    dist_mods = 5 * np.log10(d) - 5
+
+    plt.figure(figsize=(15, 5))
+
+    for ii, dist_mod in enumerate(dist_mods):  # GC=14.5, LMC=18.5, Leo I=21.5, M31=24.5
+
+        dist = 10 ** (1 + dist_mod / 5)
+        cluster.x = x_orig * dist_orig / dist
+        cluster.y = y_orig * dist_orig / dist
+        cluster.weight = f_orig * (dist_orig / dist)**2
+
+        cluster.apply_optical_train(opt, fpa)
+        hdu = fpa.read_out(OBS_EXPTIME=exptime, filename=f"{int(dist)}kpc_{int(mass)}Msun.fits")
+
+        if show:
+            plt.subplot(1, 5, ii+1)
+            plt.imshow(hdu[0].data, norm=LogNorm(), vmin=1.61E5, vmax=1.8E5)
+
+    if show:
+        plt.show()
 
 
+def plot_cluster_hdus():
+    from glob import glob
+    from astropy.io import fits
 
+    dists = [8.5, 20, 50, 200, 800]
+
+    fnames = glob("*Msun.fits")
+    jj = np.argsort([int(f.split("kpc")[0]) for f in fnames])
+
+    plt.figure(figsize=(15, 6.03))
+    for ii, fname in enumerate(np.array(fnames)[jj]):
+        cmap = "hot"
+        im = fits.getdata(fname)
+        vmin = np.min(im)
+        vmax = 100 * np.median(im)
+
+        plt.subplot(2, 5, 1 + ii)
+        plt.imshow(im, norm=LogNorm(), vmin=vmin, vmax=vmax,
+                   cmap=cmap, origin="lower")
+
+        plt.text(200, 3700, f"{dists[ii]} kpc", color="w",
+                 horizontalalignment="left", fontsize=14)
+        plt.axis("off")
+
+        plt.plot([1, 1798, 1798, 2298, 2298, 1798, 2298, 4095],
+                 [1, 2298, 1798, 1798, 2298, 2298, 2298, 1], c="w", lw=1)
+
+        if ii == 4:
+            plt.plot([3840, 3840], [2048-1250, 2048+1250], c="w", lw=3)
+            plt.text(3760, 2048, '10"', fontsize=14, color="w",
+                     horizontalalignment="right", verticalalignment="center")
+
+        m, d = 2048, 250
+        plt.subplot(2, 5, 6 + ii)
+        plt.imshow(im[m - d:m + d, m - d:m + d], norm=LogNorm(), vmin=vmin,
+                   vmax=vmax, cmap=cmap, origin="lower")
+
+        plt.axis("off")
+        if ii == 4:
+            plt.plot([480, 480], [125, 375], c="w", lw=3)
+            plt.text(470, 250, '1"', fontsize=14, color="w",
+                     horizontalalignment="right", verticalalignment="center")
+
+    plt.subplots_adjust(wspace=0., hspace=0.)
+
+# make_cluster_hdus(exptime=60)
+plot_cluster_hdus()
+
+plt.savefig("5_clusters.png", format="png")
+plt.savefig("5_clusters.pdf", format="pdf")
+
+plt.show()
